9e69d8bf09f8a76860483190fe0777a0962458dd
Denilson Santos <denilson.avsantos@hotmail.com>
2020-03-25 20:52:28 -0300

    Adicionando e estilizando pagina de cadastro ONG

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-// import { FiLogIn } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import { FiLogIn } from "react-icons/fi";
 
 import "./styles.css";
 
@@ -17,10 +18,10 @@ export default function Logon() {
           <button className="button" type="submit">
             Entrar
           </button>
-          <a href="/register">
-            {/* <FiLogIn size="16" color="#E02041" /> */}
+          <Link className="back-link" to="/register">
+            <FiLogIn size="16" color="#E02041" />
             Não tenho cadastro
-          </a>
+          </Link>
         </form>
       </section>
       <img src={heroesImg} alt="Heroes" />
